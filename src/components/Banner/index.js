ea305ac244ca75ff11175999dc6dd50736c5e5d3
dion fananie <dionfananie@gmail.com>
2020-01-07 14:20:51 +0700

    prettier eslint-prettier

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {titleBanner,wrapperBanner} from './styles'
+import { titleBanner, wrapperBanner } from './styles';
 import workImg from '@images/work.svg';
 
-const Banner =()=> {
-    return ( <div className={wrapperBanner}>
-        <img src={workImg} width="350" alt=""/>
-        <h1 className={titleBanner}>Collaborate.</h1>
-    </div> );
-}
- 
+const Banner = () => {
+  return (
+    <div className={wrapperBanner}>
+      <img src={workImg} width="350" alt="" />
+      <h1 className={titleBanner}>Collaborate.</h1>
+    </div>
+  );
+};
+
 export default Banner;
