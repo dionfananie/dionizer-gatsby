8c2b452f9e49b9599b68d91b0e3874c189f4391e
dion fananie <dionfananie@gmail.com>
2020-01-09 08:49:08 +0700

    add gatsby_env_key revision

--- src/plugins/unsplash.js
+++ src/plugins/unsplash.js
@@ -1,7 +1,5 @@
 import Unsplash from 'unsplash-js';
 
-console.log(process.env.GATSBY_UNSPLASH_KEY);
-console.log(process.env);
 const unsplash = new Unsplash({
   accessKey: process.env.GATSBY_UNSPLASH_KEY,
 });
